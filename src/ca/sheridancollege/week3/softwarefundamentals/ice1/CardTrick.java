86415d9a37fa68ca21ed37ade44552c142ac2be6
amir <amir@192.168.2.17>
2021-02-15 19:38:20 -0500

    first commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -23,7 +23,7 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
-        
+        //hi
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
